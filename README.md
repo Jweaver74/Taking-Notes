28cd645c2227fb7a9f3244589c10d885bc99efdb
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-05-03 10:16:57 -0500

    added read file from my Quilety read me generator

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# Taking-Notes
+# Talking Notes
+  ## Badges
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)[![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white)](https://nodejs.org/en)
+
+  ## Table of Contents
+  *[license](#license)
+
+  *[title](#title)
+
+  *[usage](#usage)
+
+  *[livelink](#livelink)
+
+  *[description](#description)
+
+  *[installation](#installation)
+
+  *[tests](#tests)
+
+  *[contributing](#contributing)
+
+  *[challenge](#challenge)
+
+  *[github](#github)
+
+  *[email](#email)
+
+  ## License
+ [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  [![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white)](https://nodejs.org/en)
+
+  This project is licensed under the MIT license.
+  https://opensource.org/licenses/MIT
+
+
+  ## Title
+  Taking Notes
+
+
+  ## Usage
+  As a small business owner, I want to be able to write and save notes so that i can organize and keep track of tasks I need to complete.
+
+  ## LiveLink
+  
+
+
+  ## Description
+  Creates a note pad to be able to add new notes and delete then as they are finished.
+
+
+  ## Installation
+  Clone the Taking Notes from the repo and open in VScode. Once in VScode open new terminal and run "NPM I" to install the dependencies then "NPM start" and go to the local host that is specified.
+
+
+  ## Tests
+  No test at this time
+
+
+  ## Contributing
+  [Contributor Covenant](https://www.contributor-covenant.org/)
+  https://courses.bootcampspot.com/courses/3150/assignments/49915?module_item_id=908325, https://expressjs.com/en/guide/routing.html, SMU BCS learning Assist, 
+
+
+  ## Challenge
+  Getting the proper paths to be able to run and have the notes to be saved and deleted.  Using Heroku for the first time was a huge challenge.
+
+
+  ## GitHub
+  https://github.com/Jweaver74
+
+
+  ## Email
+  jamaweaver@yahoo.com
+
+
+  ## Questions
+  If you have any questions, please contact me at [Jason Weaver](Jamaweaver@yahoo.com) or visit my GitHub page at [Jweaver74](https://github.com/Jweaver74)
--- server.js
+++ server.js
@@ -18,18 +18,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./public"));
 
 //GET /notes - Should return the notes.html file.
-app.get("/", (req, res) => {
+app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 //GET * - Should return the index.html file
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 //GET /api/notes - Should read the db.json file and return all saved notes as JSON.
-app.get("/api/notes", (req, res) => {
-  fs.readFile("./public/notes.html", "utf8", (err, data) => {
+app.get("./api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
     const jsonData = JSON.parse(data);
     console.log(jsonData);
     res.json(jsonData);
@@ -37,21 +37,27 @@ app.get("/api/notes", (req, res) => {
 });
 
 //reads the db.json file and returns all saved notes as JSON
-const readThenAppend = (content, file) => {
+const readThenAppendToJson = (content, file) => {
   fs.readFile(file, "utf8", (err, data) => {
     if (err) {
       console.log(err);
     } else {
       const parsedData = JSON.parse(data);
       parsedData.push(content);
-      writeToNoteFile(file, parsedData);
+      writeToNewNoteToJson(file, parsedData);
     }
   });
 };
 
+//writes the new note to the db.json file
+const writeToNewNoteToJson = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
 //POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file,
 //and then return the new note to the client.
-app.post("/api/notes", (req, res) => {
+app.post("./api/notes", (req, res) => {
   const { title, text } = req.body;
   if (req.body) {
     const newNote = {
@@ -59,13 +65,21 @@ app.post("/api/notes", (req, res) => {
       text: text,
       id: uniqid(),
     };
-    readThenAppend(newNote, "./db/db.json");
+
+    readThenAppendToJson(newNote, "./db/db.json");
+
     const response = {
       status: "success",
       body: newNote,
     };
+
     res.json(response);
   } else {
     res.json("Error in posting note");
   }
 });
+
+// app.listen(PORT, () => {
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port}`)
+);
