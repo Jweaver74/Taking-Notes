2bd766cb3069e4d1b7094f51d195392db04f148c
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-05-03 09:18:12 -0500

    removed the develop folder per BCS learning assit #248802

--- server.js
+++ server.js
@@ -15,40 +15,38 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("develop/public"));
+app.use(express.static("./public"));
 
 //GET /notes - Should return the notes.html file.
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "Develop/public/notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 //GET * - Should return the index.html file
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "Develop/public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-
-
 
 //GET /api/notes - Should read the db.json file and return all saved notes as JSON.
 app.get("/api/notes", (req, res) => {
-    fs.readFile("./develop/db/db.json", "utf8", (err, data) => {
-        const jsonData = JSON.parse(data);
-        console.log(jsonData);
-        res.json(jsonData);
-    });
+  fs.readFile("./public/notes.html", "utf8", (err, data) => {
+    const jsonData = JSON.parse(data);
+    console.log(jsonData);
+    res.json(jsonData);
+  });
 });
 
 //reads the db.json file and returns all saved notes as JSON
 const readThenAppend = (content, file) => {
-    fs.readFile(file, "utf8", (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            const parsedData = JSON.parse(data);
-            parsedData.push(content);
-            writeToFile(file, parsedData);
-        }
-    });
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToNoteFile(file, parsedData);
+    }
+  });
 };
 
 //POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file,
@@ -61,7 +59,7 @@ app.post("/api/notes", (req, res) => {
       text: text,
       id: uniqid(),
     };
-    readThenAppend(newNote, "./develop/db/db.json");
+    readThenAppend(newNote, "./db/db.json");
     const response = {
       status: "success",
       body: newNote,
